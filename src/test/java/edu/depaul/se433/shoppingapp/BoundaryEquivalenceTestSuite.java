98f240167e95b36d3381c605f940cba6408a301e
mteichen <63485559+mteichen@users.noreply.github.com>
2021-03-04 11:48:28 -0600

    Added Boundary and Equivalence.

---
+++ src/test/java/edu/depaul/se433/shoppingapp/BoundaryEquivalenceTestSuite.java
@@ -0,0 +1,29 @@
+package edu.depaul.se433.shoppingapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+
+public class BoundaryEquivalenceTestSuite {
+
+  /*----------Equivalence Partitioning Test----------*/
+
+ /* @ParameterizedTest
+  @DisplayName("Testing for Strong Normal Cases")
+  @MethodSource("nextParamForStrongNormal")
+  void strongNormalTest(){
+
+  }
+
+  private static Stream<Arguments>nextParamForStrongNormal(){
+    return Stream.of(
+
+    );
+  }*/
+}
