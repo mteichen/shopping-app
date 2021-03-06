223611dc1606d63e8ea4cbdf9398a4814482f2c8
mteichen <63485559+mteichen@users.noreply.github.com>
2021-03-06 02:08:32 -0600

    Added Equivalence and Boundary

---
+++ src/test/java/edu/depaul/se433/shoppingapp/testMockTotalCostCalculator.java
@@ -0,0 +1,151 @@
+package edu.depaul.se433.shoppingapp;
+
+import static edu.depaul.se433.shoppingapp.ShippingType.NEXT_DAY;
+import static edu.depaul.se433.shoppingapp.ShippingType.STANDARD;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/*
+Testing of TotalCostCalculator.calculate(ShoppingCart cart, String state, ShippingType shipping)
+                                    returns Bill(initialCost,shippingCost,tax, total)
+------------------------------------------------------------------------------------------------
+Test suite executes the following tests:
+  -Equivalence Partition testing
+    -Strong/Normal
+    -Weak/Robust
+  -Boundary testing
+    -normal
+
+                             ------Equivalence Classes------
+
+              Variable                 Class             Representative Value
+              ------------------------------------------------------------------
+                                isFreeStandardShip
+              Valid                    >50(free standard)       75.00
+                                       0.01-50(standard costs)  25.00
+              Invalid                  <=0                     -20.00
+              ------------------------------------------------------------------
+                                stateTax
+              Valid                   6% tax (IL)               IL
+                                      6% tax (CA)               CA
+                                      6% tax (NY)               NY
+                                      no state tax              WI
+              Invalid                 not a state               XX
+                                      no state                  null
+              ------------------------------------------------------------------
+                                shipping type
+              Valid                   standard                  STANDARD
+                                      next day                  NEXT_DAY
+              Invalid                 no type                   null
+              ------------------------------------------------------------------
+*/
+/*                             ---Normal Boundary Test Cases---
+
+  Desc                                initialCost         state         shipping        expected
+  High-Cost Expensive(No max value)   1000.00              WI             STANDARD        Bill(1000.00, 0, 0, 1000.00)
+  High-Cost Nominal                   75.00                WI             STANDARD        Bill(75.00, 0, 0, 75.00)
+  High-Cost Min+1                     50.02                WI             STANDARD        Bill(50.02, 0, 0, 50.02)
+  High-Cost Min                       50.01                WI             STANDARD        Bill(50.01, 0, 0, 50.01)
+   Low-Cost Max                       50.00                WI             STANDARD        Bill(50.00, 10.00, 0, 60.00)
+   Low-Cost Max-1                     49.99                WI             STANDARD        Bill(49.99, 10.00, 0, 59.99)
+   Low-Cost Nominal                   25.00                WI             STANDARD        Bill(25.00, 10.00, 0, 35.00)
+   Low-Cost Min+1                     0.02                 WI             STANDARD        Bill(0.02, 10.00, 0, 10.02)
+   Low-Cost Min                       0.01                 WI             STANDARD        Bill(0.01, 10.00, 0, 10.01)
+
+----------------------------------------------------------------------------------------------------
+*/
+
+public class testMockTotalCostCalculator {
+
+  private TotalCostCalculator totalCalculator;
+  private ShoppingCart mockCart;
+
+  /*Equivalence Class Testing*/
+
+  @BeforeEach
+  void setup(){
+    totalCalculator = new TotalCostCalculator();
+    mockCart = mock(ShoppingCart.class);
+  }
+
+  @ParameterizedTest
+  @DisplayName("Strong Normal Equivalence Tests")
+  @MethodSource("nextStrongNormal")
+  void strongNormalTest(double initialCost, String state, ShippingType shipping, Bill expected){
+    when(mockCart.cost()).thenReturn(initialCost);
+    Bill actual = totalCalculator.calculate(mockCart, state, shipping);
+    assertEquals(expected, actual);
+  }
+
+  private static Stream<Arguments>nextStrongNormal(){
+    return Stream.of(
+        Arguments.of(75.00, "IL", STANDARD, new Bill(75.00, 0, 4.50, 79.50)),
+        Arguments.of(75.00, "IL", NEXT_DAY, new Bill(75.00, 25.00, 4.50, 104.50)),
+        Arguments.of(75.00, "CA", STANDARD, new Bill(75.00, 0, 4.50, 79.50)),
+        Arguments.of(75.00, "CA", NEXT_DAY, new Bill(75.00, 25.00, 4.50, 104.50)),
+        Arguments.of(75.00, "NY", STANDARD, new Bill(75.00, 0, 4.50, 79.50)),
+        Arguments.of(75.00, "NY", NEXT_DAY, new Bill(75.00, 25.00, 4.50, 104.50)),
+        Arguments.of(75.00, "WI", STANDARD, new Bill(75.00, 0, 0, 75.00)),
+        Arguments.of(75.00, "WI", NEXT_DAY, new Bill(75.00, 25.00, 0, 100.00)),
+        Arguments.of(25.00, "IL", STANDARD, new Bill(25.00, 10.00, 1.50, 36.50)),
+        Arguments.of(25.00, "IL", NEXT_DAY, new Bill(25.00, 25.00, 1.50, 51.50)),
+        Arguments.of(25.00, "CA", STANDARD, new Bill(25.00, 10.00, 1.50, 36.50)),
+        Arguments.of(25.00, "CA", NEXT_DAY, new Bill(25.00, 25.00, 1.50, 51.50)),
+        Arguments.of(25.00, "NY", STANDARD, new Bill(25.00, 10.00, 1.50, 36.50)),
+        Arguments.of(25.00, "NY", NEXT_DAY, new Bill(25.00, 25.00, 1.50, 51.50)),
+        Arguments.of(25.00, "WI", STANDARD, new Bill(25.00, 10.00, 0, 35.00)),
+        Arguments.of(25.00, "WI", NEXT_DAY, new Bill(25.00, 25.00, 0, 50.00))
+    );
+  }
+
+  @ParameterizedTest
+  @DisplayName("Weak Robust Equivalence Tests")
+  @MethodSource("nextWeakRobust")
+  void weakRobustTest(double initialCost, String state, ShippingType shipping){
+    when(mockCart.cost()).thenReturn(initialCost);
+    assertThrows(RuntimeException.class, () -> totalCalculator.calculate(mockCart, state, shipping));
+  }
+
+  private static Stream<Arguments>nextWeakRobust(){
+    return Stream.of(
+        Arguments.of(-20.00, "WI", STANDARD),
+        Arguments.of(75.00, "XX", STANDARD),
+        Arguments.of(25.00, null, NEXT_DAY),
+        Arguments.of(25.00, "IL", null)
+    );
+  }
+
+      /*Boundary Testing*/
+
+  @ParameterizedTest
+  @DisplayName("Normal Boundary Tests")
+  @MethodSource("nextBoundary")
+  void boundaryTest(double initialCost, String state, ShippingType shipping, Bill expected){
+    when(mockCart.cost()).thenReturn(initialCost);
+    Bill actual = totalCalculator.calculate(mockCart, state, shipping);
+    assertEquals(expected, actual);
+  }
+
+  private static Stream<Arguments>nextBoundary(){
+    return Stream.of(
+        Arguments.of(1000.00, "WI", STANDARD, new Bill(1000.00, 0, 0, 1000.00)),
+        Arguments.of(75.00, "WI", STANDARD, new Bill(75.00, 0, 0, 75.00)),
+        Arguments.of(50.02, "WI", STANDARD, new Bill(50.02, 0, 0, 50.02)),
+        Arguments.of(50.01, "WI", STANDARD, new Bill(50.01, 0, 0, 50.01)),
+        Arguments.of(50.00, "WI", STANDARD, new Bill(50.00, 10.00, 0, 60.00)),
+        Arguments.of(49.99, "WI", STANDARD, new Bill(49.99, 10.00, 0, 59.99)),
+        Arguments.of(25.00, "WI", STANDARD, new Bill(25.00, 10.00, 0, 35.00)),
+        Arguments.of(0.02, "WI", STANDARD, new Bill(0.02, 10.00, 0, 10.02)),
+        Arguments.of(0.01, "WI", STANDARD, new Bill(0.01, 10.00, 0, 10.01))
+    );
+  }
+
+}
---
+++ src/test/java/edu/depaul/se433/shoppingapp/testTotalCostCalculator.java
@@ -0,0 +1,146 @@
+package edu.depaul.se433.shoppingapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static edu.depaul.se433.shoppingapp.ShippingType.STANDARD;
+import static edu.depaul.se433.shoppingapp.ShippingType.NEXT_DAY;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+
+/*
+ Testing of TotalCostCalculator.calculate(double initialCost, String state, ShippingType shipping)
+                                     returns double
+----------------------------------------------------------------------------------------------------
+    Test suite executes the following tests:
+      -Equivalence Partition testing
+        -Strong/Normal
+        -Weak/Robust
+      -Boundary testing
+        -normal
+
+                             ------Equivalence Classes------
+
+              Variable                 Class             Representative Value
+              ------------------------------------------------------------------
+                                isFreeStandardShip
+              Valid                    >50(free standard)       75.00
+                                       0.01-50(standard costs)  25.00
+              Invalid                  <=0                     -20.00
+              ------------------------------------------------------------------
+                                stateTax
+              Valid                   6% tax (IL)               IL
+                                      6% tax (CA)               CA
+                                      6% tax (NY)               NY
+                                      no state tax              WI
+              Invalid                 not a state               XX
+                                      no state                  null
+              ------------------------------------------------------------------
+                                shipping type
+              Valid                   standard                  STANDARD
+                                      next day                  NEXT_DAY
+              Invalid                 no type                   null
+              ------------------------------------------------------------------
+*/
+/*                             ---Normal Boundary Test Cases---
+
+  Desc                                initialCost         state         shipping        expected
+  High-Cost Expensive(No max value)   1000.00              WI             STANDARD        1000.00
+  High-Cost Nominal                   75.00                WI             STANDARD        75.00
+  High-Cost Min+1                     50.02                WI             STANDARD        50.02
+  High-Cost Min                       50.01                WI             STANDARD        50.01
+   Low-Cost Max                       50.00                WI             STANDARD        60.00
+   Low-Cost Max-1                     49.99                WI             STANDARD        59.99
+   Low-Cost Nominal                   25.00                WI             STANDARD        35.00
+   Low-Cost Min+1                     0.02                 WI             STANDARD        10.02
+   Low-Cost Min                       0.01                 WI             STANDARD        10.01
+
+----------------------------------------------------------------------------------------------------
+*/
+
+public class testTotalCostCalculator {
+
+  private TotalCostCalculator totalCalculator;
+
+  /*Equivalence Class Testing*/
+
+  @BeforeEach
+  void setup(){
+    totalCalculator = new TotalCostCalculator();
+  }
+
+  @ParameterizedTest
+  @DisplayName("Strong Normal Equivalence Tests")
+  @MethodSource("nextStrongNormal")
+  void strongNormalTest(double initialCost, String state, ShippingType shipping, double expected){
+    double actual = totalCalculator.calculate(initialCost, state, shipping);
+    assertEquals(expected, actual);
+  }
+
+  private static Stream<Arguments>nextStrongNormal(){
+    return Stream.of(
+        Arguments.of(75.00, "IL", STANDARD, 79.50),
+        Arguments.of(75.00, "IL", NEXT_DAY, 104.50),
+        Arguments.of(75.00, "CA", STANDARD, 79.50),
+        Arguments.of(75.00, "CA", NEXT_DAY, 104.50),
+        Arguments.of(75.00, "NY", STANDARD, 79.50),
+        Arguments.of(75.00, "NY", NEXT_DAY, 104.50),
+        Arguments.of(75.00, "WI", STANDARD, 75.00),
+        Arguments.of(75.00, "WI", NEXT_DAY, 100.00),
+        Arguments.of(25.00, "IL", STANDARD, 36.50),
+        Arguments.of(25.00, "IL", NEXT_DAY, 51.50),
+        Arguments.of(25.00, "CA", STANDARD, 36.50),
+        Arguments.of(25.00, "CA", NEXT_DAY, 51.50),
+        Arguments.of(25.00, "NY", STANDARD, 36.50),
+        Arguments.of(25.00, "NY", NEXT_DAY, 51.50),
+        Arguments.of(25.00, "WI", STANDARD, 35.00),
+        Arguments.of(25.00, "WI", NEXT_DAY, 50.00)
+    );
+  }
+
+  @ParameterizedTest
+  @DisplayName("Weak Robust Equivalence Tests")
+  @MethodSource("nextWeakRobust")
+  void weakRobustTest(double initialCost, String state, ShippingType shipping){
+    assertThrows(RuntimeException.class, () -> totalCalculator.calculate(initialCost, state, shipping));
+  }
+
+  private static Stream<Arguments>nextWeakRobust(){
+    return Stream.of(
+        Arguments.of(-20.00, "WI", STANDARD),
+        Arguments.of(75.00, "XX", STANDARD),
+        Arguments.of(25.00, null, NEXT_DAY),
+        Arguments.of(25.00, "IL", null)
+    );
+  }
+
+        /*Boundary Testing*/
+
+  @ParameterizedTest
+  @DisplayName("Normal Boundary Tests")
+  @MethodSource("nextBoundary")
+  void boundaryTest(double initialCost, String state, ShippingType shipping, double expected){
+    double actual = totalCalculator.calculate(initialCost, state, shipping);
+    assertEquals(expected, actual);
+  }
+
+  private static Stream<Arguments>nextBoundary(){
+    return Stream.of(
+        Arguments.of(1000.00, "WI", STANDARD, 1000.00),
+        Arguments.of(75.00, "WI", STANDARD, 75.00),
+        Arguments.of(50.02, "WI", STANDARD, 50.02),
+        Arguments.of(50.01, "WI", STANDARD, 50.01),
+        Arguments.of(50.00, "WI", STANDARD, 60.00),
+        Arguments.of(49.99, "WI", STANDARD, 59.99),
+        Arguments.of(25.00, "WI", STANDARD, 35.00),
+        Arguments.of(0.02, "WI", STANDARD, 10.02),
+        Arguments.of(0.01, "WI", STANDARD, 10.01)
+    );
+  }
+
+}
